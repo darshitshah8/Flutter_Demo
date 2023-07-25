62aedb3779aca1e1c2388d156cbdd5cda1275157
darshitshah8 <justengineerthings21@gmail.com>
2023-07-25 19:09:33 +0530

    Flutter list tile
    Contains Leading , title , subtitle , trailing

---
+++ flutter_listtile/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key : key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var names = ['Tony','Bruce','Thor','Natasha','Gamore','Groot','Rocket','Loky','Peter','Clynt'];
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Container"),
+
+        ),
+        body: ListView.separated(itemBuilder: (context, index){
+          return ListTile(
+            leading: Text("${index+1}"),
+            title: Text(names[index]),
+            subtitle: Text("Avengers"),
+            trailing: Icon(Icons.add_box_outlined),
+          );
+        },
+          itemCount: names.length,
+          separatorBuilder: (context, index
+              ){
+            return Divider(height:20,thickness: 1,);
+          },
+          scrollDirection: Axis.vertical,
+        )
+
+    );
+  }
+}
