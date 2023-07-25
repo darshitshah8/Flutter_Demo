1db2c936794687a4287cca8d6bd63004da6ef494
darshitshah8 <justengineerthings21@gmail.com>
2023-07-25 23:31:42 +0530

    Date time widget

---
+++ flutter_datetime/android/app/src/main/kotlin/com/example/flutter_datetime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_datetime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_datetime/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key : key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    var time = DateTime.now();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Container"),
+
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Current time  is ${time.hour} : ${time.minute} : ${time.second}"),
+          SizedBox(height: 20,),
+          ElevatedButton(
+            onPressed: (){
+              setState(() {
+              });
+            },
+            child: Text("Check"),
+          ),
+        ],
+      ),
+
+
+    );
+  }
+}
