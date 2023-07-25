bf4afa582d8714deb6b4becf5655724e58442721
darshitshah8 <justengineerthings21@gmail.com>
2023-07-25 19:52:03 +0530

    Flutter CircleAvatar Widget

---
+++ flutter_circleavatar/android/app/src/main/kotlin/com/example/flutter_circleavatar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_circleavatar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_circleavatar/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key : key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Circle Aavtar"),
+      ),
+      body: Center(
+        child: CircleAvatar(
+          backgroundImage: AssetImage('assets/images/boy.png'),
+          backgroundColor: Colors.white,
+          radius: 50,
+        ),
+      )
+
+    );
+  }
+}
